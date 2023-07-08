c67035d893826b34354baf087fdb3241eec44503
Javier de Arcos <fj.dearcos@gmail.com>
2023-07-08 16:02:01 +0200

    Add validation to create and update DTOs

--- src/pending-item/dto/create-pending-item.dto.ts
+++ src/pending-item/dto/create-pending-item.dto.ts
@@ -1,6 +1,19 @@
+import { IsNotEmpty, IsOptional, IsPositive, IsString, MaxLength, MinLength } from "class-validator";
+
 export class CreatePendingItemDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100, { message: 'Name must have less than 100 characters'})
   name: string;
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(10, { message: 'Description must have at least 10 characters'})
+  @MaxLength(1024, { message: 'Description must have less than 1024 characters'})
   description?: string;
+  @IsPositive()
   numberOfHours: number;
+  @IsOptional()
+  @IsPositive()
   cost?: number;
 }
--- src/pending-item/dto/update-pending-item.dto.ts
+++ src/pending-item/dto/update-pending-item.dto.ts
@@ -1,5 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePendingItemDto } from './create-pending-item.dto';
+import { IsEnum } from 'class-validator';
 
 export class UpdatePendingItemDto extends PartialType(CreatePendingItemDto) {
   status: string;
