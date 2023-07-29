e4eca2228384ed5e8bf383c2595f146a57a7426e
Javier de Arcos <fj.dearcos@gmail.com>
2023-07-29 20:18:16 +0200

    Create database module with datasource and configuration and configure pendingItems to be stored

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PendingItemsModule } from './pending-item/pending-items.module';
+import { DatabaseModuleModule } from './database-module/database-module.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    PendingItemsModule],
+    PendingItemsModule,
+    DatabaseModuleModule],
   controllers: [],
   providers: [],
 })
--- src/pending-item/entities/pending-item.entity.ts
+++ src/pending-item/entities/pending-item.entity.ts
@@ -1,16 +1,32 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { Status } from './status.enum';
 
+@Entity()
 export class PendingItem {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
   userId: string;
+  @Column('text')
   name: string;
+  @Column('text')
   description?: string;
+  @Column('numeric')
   numberOfHours: number;
+  @Column('numeric')
   cost?: number;
+  @Column({
+    type: 'enum',
+    enum: [Status.TO_DO, Status.IN_PROGRESS, Status.FINISHED]
+  })
   status: Status;
+  @Column('boolean')
   prioritized: boolean;
+  @Column('date')
   createdAt: Date;
+  @Column('date')
   modifiedAt?: Date;
+  @Column('date')
   prioritizedAt?: Date;
+  @Column('date')
   finishedAt?: Date;
 }
